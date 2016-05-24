9b3da8cebb1c9e995bafafab16b9cd20a1026477
Felipe Figueiredo <philsf79@gmail.com>
2016-05-24 17:04:37 -0300

    estrutura (rstudio)
    Criando labels para as seções do script

--- Aulas/BE-Associacao_II.R
+++ Aulas/BE-Associacao_II.R
@@ -3,10 +3,13 @@
 ## Dados ####
 x <- c(17.9, 18.3, 18.3, 18.4, 18.4, 20.2, 20.3, 21.8, 21.9, 22.1, 23.1, 24.2, 24.4)
 y <- c(250, 220, 145, 115, 230, 200, 330, 400, 370, 260, 270, 530, 375)
+
+## Dispersão ####
 plot(y~x, pch=20, xlim = c(16,27), ylim = c(0,600), xlab="", ylab="")
 leg.txt <- paste("r = ", round(cor(x,y), digits = 2))
 legend("topleft",legend = leg.txt, border = "")
 
+## Correlação ####
 cor(x,y)
 cor.test(x,y)
 
