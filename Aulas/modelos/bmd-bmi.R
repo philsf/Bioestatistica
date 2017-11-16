5072da750c87e563efacff216c1c46fa41dc20cc
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-11-16 17:07:49 -0200

    exemplo do paper selecionado

---
+++ Aulas/modelos/bmd-bmi.R
@@ -0,0 +1,39 @@
+
+# dados simulados ---------------------------------------------------------
+
+set.seed(1)
+BMI <- c(rnorm(50, 15, 1), rnorm(50, 20, 1), rnorm(50, 25, 1), rnorm(50, 35, 4))
+BMD <- -3*BMI - rnorm(200, 0, 20) + 100
+dados <- data.frame(BMI,BMD)
+
+modelo <- lm(BMD ~ BMI, data = dados)
+pred <- predict(modelo, interval = "conf")
+pred28 <- predict(modelo, newdata = data.frame(BMI=28))
+
+# png("Aulas/modelos/pratica-plot1.png")
+# with(dados, plot(BMI,BMD, main = "BMI x BMD", xlab = "BMI (kg/m2)", ylab = "BMD (escala ficitícia)"))
+# dev.off()
+# 
+# png("Aulas/modelos/pratica-plot2.png")
+# with(dados, plot(BMI,BMD, main = "BMI x BMD", xlab = "BMI (kg/m2)", ylab = "BMD (escala ficitícia)"))
+# abline(modelo, col = "blue", lwd = 2)
+# dev.off()
+# 
+# png("Aulas/modelos/pratica-plot3.png")
+# with(dados, plot(BMI,BMD, main = "BMI x BMD", xlab = "BMI (kg/m2)", ylab = "BMD (escala ficitícia)"))
+# abline(modelo, col = "blue", lwd = 2)
+# abline(v = 28, lty = 2, lwd = 2, col = "red")
+# dev.off()
+
+library(ggplot2)
+b <- ggplot(dados, aes(BMI,BMD)) +
+  geom_point() +
+  ggtitle("BMI x BMD")
+ggsave("Aulas/modelos/pratica-plot1.png")
+
+bsmooth <- b + geom_smooth(method = "lm")
+ggsave("Aulas/modelos/pratica-plot2.png")
+
+bsmooth + geom_vline(xintercept = 28, lty = 2, lwd =1, col = "red")
+ggsave("Aulas/modelos/pratica-plot3.png")
+  
