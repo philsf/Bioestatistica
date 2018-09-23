4a7b519a06c35f8259fd4138674e1bd3828ad02a
Felipe Figueiredo <philsf79@gmail.com>
2018-09-22 23:54:17 -0300

    histogramas

--- Aulas/Cap23-25/exercicio.R
+++ Aulas/Cap23-25/exercicio.R
@@ -2,7 +2,11 @@ ob <- read.csv("Aulas/Cap23-25/obesidade-teste-t.csv")
 
 ob2 <- tidyr::gather(ob, Grupo, Perda)
 
-png("Aulas/Cap23-25/obesidade-hist.png")
+png("Aulas/Cap23-25/obesidade-hist1.png")
+hist(ob2$Perda, main = "Grupo 1 + 2", xlab = "Perda (lbs)")
+dev.off()
+
+png("Aulas/Cap23-25/obesidade-hist2.png", 600, 400)
 par(mfrow = c(1, 2))
 hist(ob$Grupo1, main = "Grupo 1", xlab = "Perda (lbs)")
 hist(ob$Grupo2, main = "Grupo 2", xlab = "Perda (lbs)")
