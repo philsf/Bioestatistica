1491221a729a576277a24faae1285339da786ee2
Felipe Figueiredo <philsf79@gmail.com>
2018-10-30 08:16:39 -0300

    dados fixos (simulação em script a parte) - RLS e RLM

--- Aulas/Cap18-19/bmd-bmi.R
+++ Aulas/Cap18-19/bmd-bmi.R
@@ -2,28 +2,11 @@ library(philsfmisc)
 library(data.table)
 
 # dados simulados ---------------------------------------------------------
+dados.rls <- fread("Aulas/Cap18-19/dados-rls.csv")
+dados.rls.het <- fread("Aulas/Cap18-19/dados-rls-het.csv")
+dados.rlm <- fread("Aulas/Cap31-32/dados-rlm.csv")
 
-set.seed(1)
-BMI <- c(rnorm(100, 30, 5), rnorm(100, 50, 5))
-idade <- rnorm(200, 60, 5)
-horm <- round(sin(BMI))
-scatter <- rnorm(200, 0, 20)
-BMD <- -3*BMI - idade + 400 + scatter
-dados <- data.table(BMI, BMD, idade, horm)
-
-BMD2 <- -3*BMI -
-  c(
-    rnorm(50, 0, 10), # introduzir heterocedasticidade
-    rnorm(50, 0, 20),
-    rnorm(50, 0, 40),
-    rnorm(50, 0, 45)
-  ) + 300
-heterocedasticidade <-  data.table(BMI, BMD2)
-
-BMD3 <- -7*BMI - 10*idade - 100*horm + scatter + 1200
-heterocedasticidade2 <-  data.table(BMI, BMD3)
-
-modelo <- lm(BMD ~ BMI, data = dados)
+modelo <- lm(BMD ~ BMI, data = dados.rls)
 print(summary(modelo))
 format.interval(confint(modelo)[1, ]) # IC intercept
 format.interval(confint(modelo)[2, ]) # IC slope
@@ -37,9 +20,9 @@ format.interval(pred39[2:3], 1)
 # graficos ----------------------------------------------------------------
 
 library(ggplot2)
-b <- ggplot(dados, aes(BMI, BMD)) +
+b <- ggplot(dados.rls, aes(BMI, BMD)) +
   geom_point() +
-  xlim(range(BMI)) + ylim(range(BMD)) +
+  xlim(range(dados.rls$BMI)) + ylim(range(dados.rls$BMD)) +
   xlab("BMI (kg/m2)") + ylab("BMD (escala ficitícia)") +
   theme_bw() +
   ggtitle("BMI x BMD")
@@ -51,9 +34,9 @@ ggsave("Aulas/Cap18-19/pratica-plot2.png", h = 7, w = 7)
 bsmooth + geom_vline(xintercept = 39, lty = 2, lwd =1, col = "red")
 ggsave("Aulas/Cap18-19/pratica-plot3.png", h = 7, w = 7)
 
-bsmooth.only <- ggplot(dados, aes(BMI, BMD)) +
+bsmooth.only <- ggplot(dados.rls, aes(BMI, BMD)) +
   geom_smooth(method = "lm") +
-  xlim(range(BMI)) + ylim(range(BMD)) +
+  xlim(range(dados.rls$BMI)) + ylim(range(dados.rls$BMD)) +
   xlab("BMI (kg/m2)") + ylab("BMD (escala ficitícia)") +
   theme_bw() +
   ggtitle("BMI x BMD")
@@ -69,17 +52,17 @@ png("Aulas/Cap18-19/pratica-hist-resid.png")
 hist(residuals(modelo), col = "gray", main = "Distribuição dos resíduos", xlab = "")
 dev.off()
 
-b2 <- ggplot(heterocedasticidade, aes(BMI, BMD2)) +
+b2 <- ggplot(dados.rls.het, aes(BMI, BMD2)) +
   geom_point() +
   geom_smooth(method = "lm") +
-  xlim(range(BMI)) + ylim(range(BMD2)) +
+  xlim(range(dados.rls.het$BMI)) + ylim(range(dados.rls.het$BMD2)) +
   xlab("BMI (kg/m2)") + ylab("BMD (escala ficitícia)") +
   ggtitle("BMI x BMD")
 ggsave("Aulas/Cap18-19/pratica-plot-heterocedasticidade.png", h = 7, w = 7)
 
 # heterocedasticidade -----------------------------------------------------
 
-modelo2 <- lm(BMD2 ~ BMI, heterocedasticidade)
+modelo2 <- lm(BMD2 ~ BMI, dados.rls.het)
 print(summary(modelo2))
 b2.res <- ggplot(data.frame(Fitted = fitted(modelo2), Residuals = residuals(modelo2)), aes(Fitted, Residuals)) +
   geom_point() +
@@ -87,15 +70,15 @@ b2.res <- ggplot(data.frame(Fitted = fitted(modelo2), Residuals = residuals(mode
   ggtitle("Valores ajustados x Resíduos")
 ggsave("Aulas/Cap18-19/pratica-plot-heterocedasticidade-resid.png", h = 7, w = 7)
 
-b3 <- ggplot(heterocedasticidade2, aes(BMI, BMD3)) +
+b3 <- ggplot(dados.rlm, aes(BMI, BMD)) +
   geom_point() +
   geom_smooth(method = "lm") +
   xlab("BMI (kg/m2)") + ylab("BMD (escala ficitícia)") +
-  xlim(range(BMI)) + ylim(range(BMD3)) +
+  xlim(range(dados.rlm$BMI)) + ylim(range(dados.rlm$BMD)) +
   ggtitle("BMI x BMD")
 ggsave("Aulas/Cap18-19/pratica-plot-heterocedasticidade-sin.png", h = 7, w = 7)
 
-modelo3 <- lm(BMD3 ~ BMI, heterocedasticidade2)
+modelo3 <- lm(BMD ~ BMI, dados.rlm)
 print(summary(modelo3))
 b3.res <- ggplot(data.frame(Fitted = fitted(modelo3), Residuals = residuals(modelo3)), aes(Fitted, Residuals)) +
   geom_point() +
@@ -106,16 +89,16 @@ ggsave("Aulas/Cap18-19/pratica-plot-heterocedasticidade-sin-resid.png", h = 7, w
 # obsoleto ----------------------------------------------------------------
 
 # png("Aulas/modelos/pratica-plot1.png")
-# with(dados, plot(BMI,BMD, main = "BMI x BMD", xlab = "BMI (kg/m2)", ylab = "BMD (escala ficitícia)"))
+# with(dados.rls, plot(BMI,BMD, main = "BMI x BMD", xlab = "BMI (kg/m2)", ylab = "BMD (escala ficitícia)"))
 # dev.off()
 # 
 # png("Aulas/modelos/pratica-plot2.png")
-# with(dados, plot(BMI,BMD, main = "BMI x BMD", xlab = "BMI (kg/m2)", ylab = "BMD (escala ficitícia)"))
+# with(dados.rls, plot(BMI,BMD, main = "BMI x BMD", xlab = "BMI (kg/m2)", ylab = "BMD (escala ficitícia)"))
 # abline(modelo, col = "blue", lwd = 2)
 # dev.off()
 # 
 # png("Aulas/modelos/pratica-plot3.png")
-# with(dados, plot(BMI,BMD, main = "BMI x BMD", xlab = "BMI (kg/m2)", ylab = "BMD (escala ficitícia)"))
+# with(dados.rls, plot(BMI,BMD, main = "BMI x BMD", xlab = "BMI (kg/m2)", ylab = "BMD (escala ficitícia)"))
 # abline(modelo, col = "blue", lwd = 2)
 # abline(v = 39, lty = 2, lwd = 2, col = "red")
 # dev.off()
---
+++ Aulas/Cap31-32/rlm.R
@@ -0,0 +1,8 @@
+library(philsfmisc)
+library(data.table)
+
+# dados simulados ---------------------------------------------------------
+
+dados.rlm <- fread("Aulas/Cap31-32/dados-rlm.csv")
+summary(dados.rlm)
+
