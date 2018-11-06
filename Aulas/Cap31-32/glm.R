01561da0e5c6f7551027075f3f062d1ff49843e6
Felipe Figueiredo <philsf79@gmail.com>
2018-11-06 12:02:42 -0200

    padronizar estrutura dos scripts

--- Aulas/Cap31-32/glm.R
+++ Aulas/Cap31-32/glm.R
@@ -7,7 +7,7 @@ dados.rlm$horm <- factor(dados.rlm$horm, levels = c("baixo", "medio", "alto"))
 dados.rlm$osteo <- relevel(dados.rlm$osteo, "Sadio")
 dados.rlm$obeso <- cut(dados.rlm$BMI, c(-Inf, 30, Inf), c("Nao Obeso", "Obeso"))
 dados.rlm$idoso <- cut(dados.rlm$BMI, c(-Inf, 60, Inf), c("Nao Idoso", "Idoso"))
-summary(glm(osteo ~ BMI + idade + horm, binomial, dados.rlm))
+summary(dados.rlm)
 
 # modelos -----------------------------------------------------------------
 
