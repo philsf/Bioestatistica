7247bee5afed93734af890504f601e2af2a4d2e8
Felipe Figueiredo <philsf79@gmail.com>
2018-11-06 16:45:20 -0200

    gráficos dos modelos 4 e 5 atualizados

--- Aulas/Cap31-32/glm.R
+++ Aulas/Cap31-32/glm.R
@@ -29,16 +29,23 @@ c(format.float(exp(coef(modelo5)[5])), format.interval(exp(confint.default(model
 
 # graficos ----------------------------------------------------------------
 
-baseplot +
+ggplot(dados.rlm, aes(idade, BMD)) +
+  xlab("idade (anos)") + ylab("BMD (escala ficitícia)") +
+  xlim(range(dados.rlm$idade)) + ylim(range(dados.rlm$BMD)) +
   geom_point(aes()) +
   geom_hline(yintercept = 500, lwd = 1, col = "red", lty = 2) +
+  annotate("text", x = min(dados.rlm$idade)*1.2, y = 492.5, label = "Osteoporose", col = "red") +
+  geom_vline(xintercept = 60, lwd = 1, col = "blue", lty = 2) +
+  annotate("text", x = 63, y = 450,  label = "Idoso", col = "blue") +
+  theme_bw() +
+  theme(legend.position = "bottom") +
   labs(subtitle = "Desfecho binário: [BMD < 500] -> Osteoporose ") +
-  annotate("text", x = min(dados.rlm$BMI)*1.2, y = 492.5, label = "Osteoporose", col = "red")
+  ggtitle("Idoso x Osteoporose")
 ggsave("Aulas/Cap31-32/pratica-glm4.png", h = 7, w = 7)
 
 baseplot +
   geom_point(aes(col = horm)) +
-  geom_hline(yintercept = 500, lwd = 1, col = "red", lty = 2) +
   labs(subtitle = "Desfecho binário: [BMD < 500] -> Osteoporose ") +
-  annotate("text", x = min(dados.rlm$BMI)*1.2, y = 492.5, label = "Osteoporose", col = "red")
+  geom_hline(yintercept = 500, lwd = 1, col = "red", lty = 2) +
+  annotate("text", x = min(dados.rlm$BMI)*1.2, y = 492.5, label = "Osteoporose", col = "red") +
 ggsave("Aulas/Cap31-32/pratica-glm5.png", h = 7, w = 7)
